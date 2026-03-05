37984a3944fbdb49284db70d431a558dc413ccde
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-05 16:16:12 +0530

    Add files via upload

---
+++ explicit.js
@@ -0,0 +1,8 @@
+console.log("SECTION 9 – EXPLICIT CONVERSION")
+
+console.log(Number("123"))
+console.log(Number("abc"))
+console.log(Number(true))
+
+console.log(Boolean(""))
+console.log(Boolean("hello"))
