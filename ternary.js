af007ca6833c2fb9f05379f54f240a5d863141c5
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-05 16:10:55 +0530

    Add files via upload

---
+++ ternary.js
@@ -0,0 +1,16 @@
+console.log("SECTION 6 – TERNARY")
+
+let age = 20
+console.log(age >= 18 ? "Adult" : "Minor")
+
+let marks = 40
+console.log(marks >= 50 ? "Pass" : "Fail")
+
+let num = 8
+console.log(num % 2 == 0 ? "Even" : "Odd")
+
+let grade = 85
+console.log(grade >= 90 ? "A" : grade >= 75 ? "B" : "C")
+
+let n1 = -5
+console.log(n1 >= 0 ? "Positive" : "Negative")
