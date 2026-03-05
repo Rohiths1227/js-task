3b9c5d37ec27ed7098aa30b097eeaec79585459b
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-05 16:03:43 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,43 @@
+console.log("SECTION 3 – ASSIGNMENT OPERATORS")
+
+let a2 = 10
+a2 += 5
+console.log(a2)
+
+let b2 = 20
+b2 -= 8
+console.log(b2)
+
+let c2 = 7
+c2 *= 3
+console.log(c2)
+
+let d2 = 40
+d2 /= 5
+console.log(d2)
+
+let e2 = 25
+e2 %= 4
+console.log(e2)
+
+let f2 = 5
+f2 += 3
+f2 *= 2
+console.log(f2)
+
+let g2 = 10
+g2 -= 2
+g2 /= 4
+console.log(g2)
+
+let h2 = 8
+h2 %= 3
+console.log(h2)
+
+let i2 = 6
+i2 += i2
+console.log(i2)
+
+let j2 = 2
+j2 *= j2 += 3
+console.log(j2)
