3032265f0866e0973c1fe253089eba669494b5c3
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-05 15:52:08 +0530

    Add files via upload

---
+++ arithmetic operator.js
@@ -0,0 +1,19 @@
+console.log("SECTION 1 – BASIC ARITHMETIC")
+
+console.log(25 + 4)
+console.log(25 - 4)
+console.log(25 * 4)
+console.log(25 / 4)
+
+console.log(100 % 30)
+console.log(7 ** 2)
+console.log(2 ** 5)
+
+let a1 = 50
+console.log(a1 / 5)
+
+console.log(45 % 6)
+console.log(0.5 * 8)
+console.log(75 - 200)
+console.log(10 + 20 + 30)
+console.log(9 % 2)
