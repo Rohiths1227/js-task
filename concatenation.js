6506e2cecb85f41ab20813654fe58c9bfb49eef8
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-05 16:13:57 +0530

    Add files via upload

---
+++ concatenation.js
@@ -0,0 +1,12 @@
+console.log("SECTION 7 – STRING CONCATENATION")
+
+console.log("Hello" + " " + "World")
+
+let first = "Rohith"
+let last = "S"
+
+console.log(first + " " + last)
+console.log(`My name is ${first} ${last}`)
+
+console.log("10" + 5)
+console.log(5 + "10")
