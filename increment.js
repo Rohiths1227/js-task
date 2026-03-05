00497706222ef7e843a5a0a3da9cfcb6a1f10baa
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-05 16:02:30 +0530

    Add files via upload

---
+++ increment.js
@@ -0,0 +1,61 @@
+console.log("SECTION 2 – INCREMENT / DECREMENT")
+
+let a = 5
+console.log(a++)
+console.log(a)
+
+let b = 5
+console.log(++b)
+console.log(b)
+
+let c = 10
+let d = c++
+console.log(c, d)
+
+let e = 10
+let f = ++e
+console.log(e, f)
+
+let g = 1
+let h = g++ + g++
+console.log(g, h)
+
+let i = 1
+let j = ++i + ++i
+console.log(i, j)
+
+let k = 3
+k = k++
+console.log(k)
+
+let l = 3
+l = ++l
+console.log(l)
+
+let x = 5
+let y = --x
+console.log(x, y)
+
+let x1 = 5
+let y1 = x1--
+console.log(x1, y1)
+
+let m = 2
+let n = m++ + ++m + m++
+console.log(m, n)
+
+let p = 4
+let q = --p + p-- + --p
+console.log(p, q)
+
+let r = 1
+let s = ++r + r++ + ++r
+console.log(r, s)
+
+let t = 5
+let u = t++ + t++ + t++
+console.log(t, u)
+
+let v = 10
+v += v++
+console.log(v)
