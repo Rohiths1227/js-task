ccf959eed534a6cf0439ac46c8f2794f62feded9
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-08 21:54:41 +0530

    Add files via upload

---
+++ do_while_loop.js
@@ -0,0 +1,36 @@
+//Print 1 to 5
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
+
+//Print 5 to 1
+let j = 5;
+do {
+  console.log(j);
+  j--;
+} while (j >= 1);
+
+//Multiples of 3 up to 30
+let k = 3;
+do {
+  console.log(k);
+  k += 3;
+} while (k <= 30);
+
+//Numbers divisible by 4
+let n = 1;
+do {
+  if (n % 4 === 0) {
+    console.log(n);
+  }
+  n++;
+} while (n <= 20);
+
+//Numbers until 10
+let x = 1;
+do {
+  console.log(x);
+  x++;
+} while (x <= 10);
