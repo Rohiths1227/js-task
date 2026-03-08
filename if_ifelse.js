1ff8ee87a275d4cf2425376516736fe5907fa3ec
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-08 21:30:23 +0530

    Add files via upload

---
+++ if_ifelse.js
@@ -0,0 +1,37 @@
+//  Number greater than 10
+let num = 15;
+if (num > 10) {
+  console.log("Number is big");
+}
+
+//  Voting eligibility
+let age = 20;
+if (age >= 18) {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not eligible to vote");
+}
+
+//  Temperature check
+let temperature = 35;
+if (temperature > 30) {
+  console.log("Hot");
+} else {
+  console.log("Cool");
+}
+
+//  Pos or Neg
+let number = -5;
+if (number >= 0) {
+  console.log("Positive");
+} else {
+  console.log("Negative");
+}
+
+//  Even or Odd
+let n = 8;
+if (n % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
+}
